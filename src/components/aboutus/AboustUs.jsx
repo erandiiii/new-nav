453907311e831e2c066f247fd35ec08e431a6bef
erandiiii <erandberisha1@gmail.com>
2024-09-03 16:57:05 +0200

    asd

--- src/components/aboutus/AboustUs.jsx
+++ src/components/aboutus/AboustUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import './AboutUs.scss'
+import './AboutUs.scss';
 
 const AboustUs = () => {
     return (
@@ -15,8 +15,34 @@ const AboustUs = () => {
             </div>
             <div className="au-right">
                 <div className="img-holder">
-                    <div className="au-card1"></div>
-                    <div className="au-card2"></div>
+                    <div className="au-card1">
+                        <div className="card1-img"></div>
+                        <div className="card1-content">
+                            <h3>Dea Gashi</h3>
+                            <div className="content-row">
+                                <p>Model</p>
+                                <span></span>
+                                <p>Photography</p>
+                                <span></span>
+                                <p>Artist</p>
+                            </div>
+                            <h3>19 may 2022</h3>
+                        </div>
+                    </div>
+                    <div className="au-card2">
+                        <div className="card2-img"></div>
+                        <div className="card2-content">
+                            <h3>Dea Gashi</h3>
+                            <div className="content2-row">
+                                <p>Model</p>
+                                <span></span>
+                                <p>Photography</p>
+                                <span></span>
+                                <p>Artist</p>
+                            </div>
+                            <h3>19 may 2022</h3>
+                        </div>
+                    </div>
                 </div>
                 <Link></Link>
             </div>
